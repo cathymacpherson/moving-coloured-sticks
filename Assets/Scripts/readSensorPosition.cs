b161cbea60820bbbad8ac533ef43586f2e8e42d0
cathymacpherson <52889256+cathymacpherson@users.noreply.github.com>
2020-05-18 13:30:13 +0800

    First Commit

---
+++ Assets/Scripts/readSensorPosition.cs
@@ -0,0 +1,57 @@
+﻿    using UnityEngine;
+    using System.Collections;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.IO;
+    using System.ComponentModel;
+
+//This script outputs the position and rotation of sensor A and sensor B
+
+    public class readSensorPosition : MonoBehaviour 
+    {
+        //Private variables
+        private GameObject sensorA; 
+        private GameObject sensorB; 
+
+        //Public variables
+        public string fileName;
+
+  
+    private void Start()
+    {
+         sensorA = GameObject.Find("/Used Sensors/Knuckle (0)");
+         sensorB = GameObject.Find("/Used Sensors/Knuckle (1)");
+     }
+
+
+    private void Update()
+    {
+
+    }
+
+  
+    private void FixedUpdate()
+
+    {
+        float[] sensorPosA = new float[] { sensorA.transform.position.x, sensorA.transform.position.y, sensorA.transform.position.z,
+                                             sensorA.transform.eulerAngles.x, sensorA.transform.eulerAngles.y, sensorA.transform.eulerAngles.z };
+        float[] sensorPosB = new float[] { sensorB.transform.position.x, sensorB.transform.position.y, sensorB.transform.position.z,
+                                             sensorB.transform.eulerAngles.x, sensorB.transform.eulerAngles.y, sensorB.transform.eulerAngles.z };
+        //Other positions previously read - Quaterions and localEulerAngles
+        var line = string.Format("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}",
+                                    DateTime.Now, 
+                                    sensorPosA[0], sensorPosA[1], sensorPosA[2], sensorPosA[3], sensorPosA[4], sensorPosA[5],
+                                    sensorPosB[0], sensorPosB[1], sensorPosB[2], sensorPosB[3], sensorPosB[4], sensorPosB[5]);
+            var name = fileName + ".txt";
+            StreamWriter writer = new StreamWriter(name, true);
+            writer.WriteLine(line);
+            {
+                if (writer != null)
+                    writer.Close(); //Check whether this is closing and re-opening the file after every line
+            }
+
+        }
+        
+    }
---
+++ Assets/Scripts/rotationController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using System.Linq;
+using UnityEngine.UI;
+
+public class rotationController : MonoBehaviour
+{
+
+    GameObject csvFile;
+    GameObject rightStickYellow;
+    GameObject leftStickYellow;
+    GameObject rightStickBlue;
+    GameObject leftStickBlue;
+    //Slider mySlider;
+    List<string> stringList = new List<string>(); //stores the read in lines from the text file
+     public int currentLine = 0;
+     private string[] data;
+     public string[] row;
+
+       
+void Start()
+{
+    
+   leftStickYellow = GameObject.Find("/L_Stick_Yellow/L_Rotator");
+    rightStickYellow = GameObject.Find("/R_Stick_Yellow/R_Rotator");
+
+        leftStickBlue = GameObject.Find("/L_Stick_Blue/L_Rotator");
+        rightStickBlue = GameObject.Find("/R_Stick_Blue/R_Rotator");
+
+
+        //mySlider = GameObject.Find("Slider").GetComponent<Slider>();
+       // mySlider.gameObject.SetActive(false);
+
+
+        readCSV();
+}
+
+
+    void readCSV()
+    {
+        TextAsset rotationData = Resources.Load<TextAsset>("high"); //loads in data
+        data = rotationData.text.Split('\n'); //splits the data into array by row
+    } 
+
+void runMovement()
+    {
+        currentLine++; //increases the line
+       // if (currentLine >= data.Length)
+        //    currentLine = 0; //loop the animation if it reaches the end
+        row = data[currentLine].Split(','); //loops through data and splits data into array by comma
+        
+        for (int i = 0; i < data.Length - 1; i++)
+        {
+           // rightStickYellow.transform.eulerAngles = new Vector3(rightStickYellow.transform.eulerAngles.x, rightStickYellow.transform.eulerAngles.y, float.Parse(row[0])*20);
+           // leftStickYellow.transform.eulerAngles = new Vector3(leftStickYellow.transform.eulerAngles.x, leftStickYellow.transform.eulerAngles.y, 0 - float.Parse(row[1])*20);
+
+              rightStickBlue.transform.eulerAngles = new Vector3(rightStickBlue.transform.eulerAngles.x, rightStickBlue.transform.eulerAngles.y, 0 - float.Parse(row[1]) * 20);
+              leftStickBlue.transform.eulerAngles = new Vector3(leftStickBlue.transform.eulerAngles.x, leftStickBlue.transform.eulerAngles.y,  float.Parse(row[0]) * 20);
+
+        }
+    }
+
+
+
+void Update()
+{
+        
+}
+
+   
+void FixedUpdate()
+{
+    runMovement(); 
+}
+}
